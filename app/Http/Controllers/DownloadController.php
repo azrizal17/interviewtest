a02c7b2e05c8af09ecb86768634d4aac33730431
Azrizal <azrizal@msd.net.my>
2019-06-04 00:19:36 +0800

    update download csv

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -8,14 +8,22 @@ use Carbon\Carbon;
 
 class DownloadController extends Controller
 {
-    
 	public function getIndex()
 	{
 		$json = Storage::disk('local')->get('orders-20190602.json');
 		$collection = json_decode($json, true);
 		$orders = collect($collection['orders']);
-		$filter = $orders->where('STATUS', 'delivered')->where('SUBTOTAL', '>', 20);	
-		$result = $filter->all();	 
+		$filter = $orders->where('STATUS', 'delivered')->where('SUBTOTAL', '>', 20);
+		$groups = $filter->groupBy('AGENT')->map(function ($agent) {
+			return $agent->groupBy('PRODUCT')->map(function ($product) {
+				return $product->groupBy(function ($ym) {
+					return Carbon::createFromFormat('Y-m-d H:i:s', $ym['ORDER_CREATED_AT'])->format('Y-m');
+				})->map(function ($year_month) {
+					return $year_month->sum('TOTAL_AWARD_POINT');
+				});
+			});
+		});
+		$result = $groups->all();
 		$filename = 'CSV-'.date('YmdHis').'.csv';
 		Storage::disk('local')->put('files/'.$filename, $this->getCSV($result));
 		echo $filename.' successfully generated!';
@@ -25,12 +33,24 @@ class DownloadController extends Controller
 	{
 		$contents = '';
 		$handle = fopen('php://temp', 'r+');
-		foreach ($data as $line) {
-			unset($line['#'], $line['ORDER_NO'], $line['CUSTOMER'], $line['STATUS'], $line['SKU'], $line['DISCOUNT'], $line['SUBTOTAL']);
-			$line['MONTH'] = Carbon::createFromFormat('Y-m-d H:i:s', $line['ORDER_CREATED_AT'])->format('F');
-			$line['YEAR'] = Carbon::createFromFormat('Y-m-d H:i:s', $line['ORDER_CREATED_AT'])->format('Y');
-			unset($line['ORDER_CREATED_AT']);
-			fputcsv($handle, $line, $delimiter, $enclosure);
+		$header = array('Agent', 'Product', 'Year', 'Month', 'Total Award Point');
+		fputcsv($handle, $header);
+		$line = [];
+		foreach ($data as $name => $value) {
+			$line['name'] = $name;
+			if (!empty($value)) {
+				foreach ($value as $item => $year_month) {
+					$line['product'] = $item;
+					if (!empty($year_month)) {
+						foreach ($year_month as $ym => $total) {
+							$line['year'] = Carbon::createFromFormat('Y-m', $ym)->format('Y');
+							$line['month'] = Carbon::createFromFormat('Y-m', $ym)->format('F');
+							$line['total'] = $total;
+							fputcsv($handle, $line, $delimiter, $enclosure);
+						}
+					}
+				}
+			}
 		}
 		rewind($handle);
 		while (!feof($handle)) {
